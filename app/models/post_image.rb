bad7583cc0a52eaabb6ac793947c028d0aa83128
Takutomys <106647199+Takutomys@users.noreply.github.com>
2022-06-28 06:22:06 +0000

    meshiterro[22]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,19 +1,22 @@
 class PostImage < ApplicationRecord
-  
+
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
-  
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
+
   def get_image
     if image.attached?
       image
     else
       'no_image.jpg'
-    end 
-  end 
-  
+    end
+  end
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -21,9 +24,9 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
-  end   
-      
+  end
+
 end
